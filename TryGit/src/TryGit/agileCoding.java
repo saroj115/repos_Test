76a8241adf61ab028d13298eb5379cf8344b7ecf
HP <HP@192.168.1.10>
2020-08-03 17:26:31 +1000

    commit

---
+++ TryGit/src/TryGit/agileCoding.java
@@ -0,0 +1,15 @@
+ package TryGit;
+
+public class agileCoding {
+
+	public agileCoding() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+ 
